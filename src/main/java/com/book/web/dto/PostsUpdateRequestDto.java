2a1040cae8c32d19779d0969c4c54684cff1e8c1
howan <oke2103@gmail.com>
2022-02-16 23:30:04 +0900

    업데이트 기능 및 테스트 코드 작성

---
+++ src/main/java/com/book/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,20 @@
+package com.book.web.dto;
+
+import com.book.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
